0bca0d1afe5cb90d5836d804052e2c329499eafe
nkusumo <nis.kusumo@gmail.com>
2021-05-23 21:25:55 -0700

    commit tree 9edd089feed3eff369e52a55bf8bfe7d9b6b687d

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(response => response.json())
+    .then(object => {
+        document.querySelector('body').append(document.createElement('div').textContent = object.id);
+    })
+    .catch(error => {
+        document.querySelector('body').append(document.createElement('div').textContent = error.message);
+    })
+}
